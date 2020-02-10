83d96ae79dcd45ae5d0c983406868597339a26e3
Jessica Heggem <jessicaheggem@gmail.com>
2020-02-10 13:32:58 -0600

    changed database name in pool and added registration inputs

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,35 +1,56 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 class RegisterPage extends Component {
   state = {
-    timestamp: '',
-    firstName: '',
-    lastName: '',
+    // firstName: '',
+    // lastName: '',
     username: '',
     password: '',
-    city: '',
-    state: '',
-    occupation: '',
-    portfolioUrl: '',
-    timeAvailable: '',
-    languages: '',
-    qualifications: '',
+    // city: '',
+    // state: '',
+    // occupation: '',
+    // portfolioUrl: '',
+    // timeAvailable: '',
+    // languages: '',
+    // qualifications: '',
   };
 
   registerUser = (event) => {
     event.preventDefault();
 
-    if (this.state.username && this.state.password) {
+    if (
+      // this.state.firstName &&
+      // this.state.lastName &&
+      this.state.username &&
+      this.state.password 
+      // &&
+      // this.state.city &&
+      // this.state.state &&
+      // this.state.occupation &&
+      // this.state.portfolioUrl &&
+      // this.state.timeAvailable &&
+      // this.state.languages &&
+      // this.state.qualifications
+      ) {
       this.props.dispatch({
         type: 'REGISTER',
         payload: {
+          // firstName: this.state.firstName,
+          // lastName: this.state.lastName,
           username: this.state.username,
           password: this.state.password,
+          // city: this.state.city,
+          // state: this.state.state,
+          // occupation: this.state.occupation,
+          // portfolioUrl: this.state.portfolioUrl,
+          // timeAvailable: this.state.timeAvailable,
+          // languages: this.state.languages,
+          // qualifications: this.state.qualifications,
         },
       });
     } else {
-      this.props.dispatch({type: 'REGISTRATION_INPUT_ERROR'});
+      this.props.dispatch({ type: 'REGISTRATION_INPUT_ERROR' });
     }
   } // end registerUser
 
@@ -52,6 +73,28 @@ class RegisterPage extends Component {
         )}
         <form onSubmit={this.registerUser}>
           <h1>Register User</h1>
+          {/* <div>
+            <label htmlFor="firstName">
+              First Name:
+              <input
+                type="text"
+                name="firstName"
+                value={this.state.firstName}
+                onChange={this.handleInputChangeFor('firstName')}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="lastName">
+              Last Name:
+              <input
+                type="text"
+                name="lastName"
+                value={this.state.lastName}
+                onChange={this.handleInputChangeFor('lastName')}
+              />
+            </label>
+          </div> */}
           <div>
             <label htmlFor="username">
               Username:
@@ -74,6 +117,83 @@ class RegisterPage extends Component {
               />
             </label>
           </div>
+          {/* <div>
+            <label htmlFor="city">
+              City:
+              <input
+                type="text"
+                name="city"
+                value={this.state.city}
+                onChange={this.handleInputChangeFor('city')}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="state">
+              State:
+              <input
+                type="text"
+                name="state"
+                value={this.state.state}
+                onChange={this.handleInputChangeFor('state')}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="occupation">
+              Occupation:
+              <input
+                type="text"
+                name="occupation"
+                value={this.state.occupation}
+                onChange={this.handleInputChangeFor('occupation')}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="portfolioUrl">
+              Portfolio URL:
+              <input
+                type="text"
+                name="portfolioUrl"
+                value={this.state.portfolioUrl}
+                onChange={this.handleInputChangeFor('portfolioUrl')}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="timeAvailable">
+              Time Available:
+              <input
+                type="text"
+                name="timeAvailable"
+                value={this.state.timeAvailable}
+                onChange={this.handleInputChangeFor('timeAvailable')}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="languages">
+              Languages:
+              <input
+                type="text"
+                name="languages"
+                value={this.state.languages}
+                onChange={this.handleInputChangeFor('languages')}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="qualifications">
+              Qualifications:
+              <input
+                type="text"
+                name="qualifications"
+                value={this.state.qualifications}
+                onChange={this.handleInputChangeFor('qualifications')}
+              />
+            </label>
+          </div> */}
           <div>
             <input
               className="register"
@@ -87,7 +207,7 @@ class RegisterPage extends Component {
           <button
             type="button"
             className="link-button"
-            onClick={() => {this.props.dispatch({type: 'SET_TO_LOGIN_MODE'})}}
+            onClick={() => { this.props.dispatch({ type: 'SET_TO_LOGIN_MODE' }) }}
           >
             Login
           </button>
