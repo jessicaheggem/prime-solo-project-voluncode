7d5ae91cd76065bd2a3a835562b0ee954c8dd20a
Jessica Heggem <jessicaheggem@gmail.com>
2020-02-13 13:59:14 -0600

    installed react popups and imported to projects & project info pages

--- src/components/AvailableProjects/AvailableProjects.js
+++ src/components/AvailableProjects/AvailableProjects.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import Popup from "reactjs-popup";
 
 
 // This is one of our simplest components
--- src/components/ProjectInfo/ProjectInfo.jsx
+++ src/components/ProjectInfo/ProjectInfo.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import Popup from "reactjs-popup";
 
 
 class ProjectInfo extends Component {
