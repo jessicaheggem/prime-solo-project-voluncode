eb506de40214c4b59c556f411c867ad6626f99d3
Jessica Heggem <jessicaheggem@gmail.com>
2020-02-17 07:45:13 -0600

    added WHERE statement to JOIN in developerProject router

--- server/routes/developerProject.router.js
+++ server/routes/developerProject.router.js
@@ -6,7 +6,9 @@ const router = express.Router();
 router.get('/', (req, res) => {
     const queryText =
         `SELECT * FROM "project"
-        JOIN "user_project" ON "project".id = "user_project".project_id;`;
+        JOIN "user_project" ON "project".id = "user_project".project_id
+        WHERE "project".user_id = $1;`;
+        // not sure if I need the WHERE statement above in order for my GET to work?
     pool.query(queryText)
         .then(result => {
             console.log(result.rows);
